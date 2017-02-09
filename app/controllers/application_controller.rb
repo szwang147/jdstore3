5dda2d3b191b212726af237bd1961dd1f0d4313e
simon <wangzd.fudan@gmail.com>
2017-02-09 14:32:03 +0800

    add controller carts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
 
     session[:cart_id] = cart.id
     return cart
-    
+
   end
 
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_items, source: :product
 
   def add_product_to_cart(product)
-    ci = cart_items.new
+    ci = cart_items.build
     ci.product = product
     ci.quantity = 1 ##本来就是默认值1啊
     ci.save

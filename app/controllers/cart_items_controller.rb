9f3917b222a54ed15f9ef7c026ed19c6018ba0d4
simon <wangzd.fudan@gmail.com>
2017-02-10 18:36:59 +0800

    cannot add the same product

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,6 +4,7 @@ class CartItemsController < ApplicationController
     def destroy
       @cart = current_cart
       @cart_item = @cart.cart_items.find(params[:id])
+      # @cart_item = @cart.cart_items.find_by_product_id(params[:id])
       @product = @cart_item.product
       @cart_item.destroy
 

83f171babaa896ff78c6f4d10ee0846df779c32c
simon <wangzd.fudan@gmail.com>
2017-02-10 18:24:22 +0800

    delete an item from cart

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ resources :carts do
   end
 end
 
-
+resources :cart_items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "products#index"
